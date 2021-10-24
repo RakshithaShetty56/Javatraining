c65511b5bd08f864cb5c307f97538d0c6772907e
SHETRAK <SHETRAK@CGSCLXR75690769.in623.corpintra.net>
2021-10-24 13:22:50 +0530

    first commit

---
+++ Java1/src/main/java/Java/Java1/App.java
@@ -0,0 +1,14 @@
+package Java.Java1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Trying to connect to github" );
+        System.out.println("May b first commit");
+    }
+}
