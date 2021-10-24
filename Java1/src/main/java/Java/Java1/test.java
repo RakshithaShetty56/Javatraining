78abf75def12f0d4ca43a2ac9a62da0c7980ed88
SHETRAK <SHETRAK@CGSCLXR75690769.in623.corpintra.net>
2021-10-24 13:25:31 +0530

    2nd commit

---
+++ Java1/src/main/java/Java/Java1/test.java
@@ -0,0 +1,10 @@
+package Java.Java1;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Trying to add new files");
+	}
+
+}
